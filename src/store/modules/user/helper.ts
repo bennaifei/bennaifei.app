9cb325f72bb7732ecef5ad7141c2b0512ecebae4
bennaifei <86400810+bennaifei@users.noreply.github.com>
2023-04-06 02:06:17 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,10 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://raw.githubusercontent.com/bennaifei/bennaifei.app/main/src/assets/
+-2126a66d1629f317.jpg',
+      name: '苯奈菲',
+      description: 'Star on <a href="https://github.com/bennaifei/bennaifei.app" class="text-blue-500" target="_blank" >Github</a>',
     },
   }
 }
